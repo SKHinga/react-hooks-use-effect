70fc96375ad1bdb9447d0eddb2a9386c25db87b3
Samuel Hinga <samuelhinga200@gmail.com>
2022-05-19 20:15:52 +0300

    Completed

--- src/DogPics.js
+++ src/DogPics.js
@@ -2,25 +2,22 @@ import React, { useState, useEffect } from "react";
 
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
-function DogPics() {
+function DogPics({ onchance }) {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    console.log("useEffect");
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
-        console.log("setState");
+        // setting state in the useEffect callback
         setImages(data.message);
       });
-  });
-
-  console.log("render");
+  }, [onchance]);
 
   return (
     <div>
       {images.map((image) => (
-        <img src={image} key={image} />
+        <img src={image} key={image} alt={image}/>
       ))}
     </div>
   );
